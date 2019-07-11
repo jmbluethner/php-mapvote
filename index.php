352096456890412bbafb520f6bd5a099b23f9a86
Jamo <develop@nighttimedev.com>
2019-07-11 11:03:20 +0200

    Added & Implemented Bootstrap

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <script src="./assets/js/functions.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  </head>
+  <body>
+    
+  </body>
+</html>
